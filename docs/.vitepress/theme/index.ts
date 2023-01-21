daae9164ecc1076ed180ea1dd2c21e0a7046d234
Masaki Koyanagi <mascii@gmail.com>
2023-01-21 11:24:30 +0900

    docs: make it possible to try `yup` and `vue-yup-form` on the console

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,22 @@
 import DefaultTheme from "vitepress/theme";
 
+import * as yup from "yup";
+import { version as yupVersion } from "yup/package.json";
+import * as vueYupForm from "../../../src/index";
+import { version as vueYupFormVersion } from "../../../package.json";
+
 import "./styles/vars.css";
 
 import CodeSandbox from "../../components/CodeSandbox.vue";
+
+Object.assign(globalThis, { yup }, vueYupForm);
+
+console.info(
+  "%cFeel free to try sample codes here",
+  "background: #222; color: #EE6A55"
+);
+console.info(`yup version: ${yupVersion}`);
+console.info(`vue-yup-form version: ${vueYupFormVersion}`);
 
 export default {
   ...DefaultTheme,
