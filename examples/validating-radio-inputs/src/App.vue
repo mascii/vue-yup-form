402a6358e6898920f2f3617d64af65fb6182646e
Masaki Koyanagi <mascii@gmail.com>
2023-02-19 11:23:15 +0900

    examples: add validating-radio-inputs for StackBlitz

---
+++ examples/validating-radio-inputs/src/App.vue
@@ -0,0 +1,66 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+
+import { defineForm, field, isValidForm, toObject } from "vue-yup-form";
+import * as yup from "yup";
+
+const generateForm = () => {
+  return defineForm({
+    drink: field<string | undefined>(
+      undefined,
+      yup.string().required("You must choose a drink")
+    ),
+  });
+};
+
+const form = generateForm();
+const submitted = ref(false);
+const onSubmit = () => {
+  submitted.value = true;
+  if (!isValidForm(form)) {
+    return;
+  }
+  alert(JSON.stringify(toObject(form), null, 2));
+};
+
+const values = computed(() => toObject(form));
+</script>
+
+<template>
+  <div>
+    <form @submit.prevent="onSubmit">
+      <input name="drink" type="radio" v-model="form.drink.$value" value="" />
+      None
+      <input
+        name="drink"
+        type="radio"
+        v-model="form.drink.$value"
+        value="Tea"
+      />
+      Tea
+      <input
+        name="drink"
+        type="radio"
+        v-model="form.drink.$value"
+        value="Coffee"
+      />
+      Coffee
+
+      <span v-if="submitted && form.drink.$error">{{
+        form.drink.$error.message
+      }}</span>
+
+      <button>Submit</button>
+
+      <p>Values</p>
+      <pre>{{ values }}</pre>
+    </form>
+  </div>
+</template>
+
+<style>
+span {
+  display: block;
+  margin: 10px 0;
+}
+</style>
